f314ff15d1b7aeaea4c3fa97112f68642f9856f2
navyarani-k <navyarani.k@etvbharat.com>
2021-09-22 12:00:23 +0530

    angular component 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,23 +5,27 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list'; 
 import { FlexLayoutModule } from '@angular/flex-layout';
 
  
 import { AppComponent } from './app.component';
 
 import 'hammerjs';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    MatListModule
 
   ],
   providers: [],
